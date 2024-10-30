8f02edd468455e0fedc2eaa01b8edd7896bc1579
Chris Parkins <cjparkins@yahoo.com>
2024-10-30 14:03:37 -0600

    Remove the locale_selector argument from the Babel __init__() call and call init_app instead.

--- privacyidea/app.py
+++ privacyidea/app.py
@@ -177,7 +177,8 @@ def create_app(config_name="development",
 
     Versioned(app, format='%(path)s?v=%(version)s')
 
-    Babel(app, locale_selector=get_accepted_language)
+    Babel(app)
+    Babel.init_app(locale_selector=get_accepted_language)
 
     # Setup logging
     log_read_func = {
