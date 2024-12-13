0dbfdf3803f47868fa024082d0c56321a2299ba0
Chris Parkins <cjparkins@yahoo.com>
2024-12-13 14:37:47 -0700

    Removing debugging.

--- privacyidea/app.py
+++ privacyidea/app.py
@@ -211,7 +211,6 @@ def create_app(config_name="development",
     if os.path.isfile(log_config_file):
         for cnf_type in ['cfg', 'yaml']:
             try:
-                print(f"Trying to read logging configuration from {log_config_file} using {cnf_type}")
                 log_read_func[cnf_type](log_config_file)
                 if not silent:
                     print('Read Logging settings from {0!s}'.format(log_config_file))
@@ -237,7 +236,6 @@ def create_app(config_name="development",
         logging.config.dictConfig(DEFAULT_LOGGING_CONFIG)
 
     log = logging.getLogger(__name__)
-    log.propagate = False
 
     queue.register_app(app)
 
